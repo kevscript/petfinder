c6ce57ce3f9e22f662633ae0033918c1c9813aff
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-07-22 18:27:01 +0200

    created styled-component theme file and GlobalStyle file

---
+++ frontend/src/styles/GlobalStyle.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Source Sans Pro", "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: ${props => props.theme.colors.textDark};
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+  
+  ul {
+    list-style: none;
+  }
+`
+
+export default GlobalStyle
---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#C9C5FF',
+    primaryDark: '#7B70FD',
+    secondary: '#FFE4BC',
+    secondaryDark: '#FFBD5A',
+    cta: '',
+    textDark: '#4B3F4E'
+  }
+}
+
+export default theme
