2abea7d016c8b56a76c11ebab6a530005ff008bd
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-07-23 16:17:44 +0200

    connected redux with withFormik HOC

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { withFormik } from 'formik'
+import { withFormik, Field } from 'formik'
 
-const MyForm = ({ content }) => {
+const MyForm = (props) => {
   const [open, setOpen] = useState(false)
-  const { loading, error, type, breeds, genders, ages, sizes } = content
+  const { loading, error, type, breeds, genders, ages, sizes } = props.content
+  const { values, touched, errors, handleChange, handleBlur, handleSubmit } = props
+
 
   const handleOptions = (e) => {
     e.preventDefault()
@@ -23,46 +25,39 @@ const MyForm = ({ content }) => {
     return (
       <div>
         <h1>Looking for a {type.name}</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="breeds">Breed</label>
-            <input type="text" id="breeds" list="breeds-list" />
-            <datalist id="breeds-list">
-              {breeds && breeds.map(breed =>
-                <option key={breed.name} value={breed.name} />
-              )}
-            </datalist>
+            {errors.breed && touched.breed && <div id="feedback">{errors.breed}</div>}
+            <Field component="select" id="breeds" onChange={handleChange} onBlur={handleBlur} name="breed" value={values.breed}>
+              {breeds && breeds.map(x => <option key={x.name} value={x.name}>{x.name}</option>)}
+            </Field>
           </div>
           <div>
             <label htmlFor="genders">Gender</label>
-            <input type="text" id="genders" list="genders-list" />
-            <datalist id="genders-list">
-              {genders && genders.map(x =>
-                <option key={x} value={x} />
-              )}
-            </datalist>
+            {errors.gender && touched.gender && <div id="feedback">{errors.gender}</div>}
+            <Field component="select" id="genders" onChange={handleChange} onBlur={handleBlur} name="gender" value={values.gender}>
+              {genders && genders.map(x => <option key={x} value={x}>{x}</option>)}
+            </Field>
           </div>
           <div>
             <label htmlFor="ages">Age</label>
-            <input type="text" id="ages" list="ages-list" />
-            <datalist id="ages-list">
-              {ages && ages.map(x =>
-                <option key={x} value={x} />
-              )}
-            </datalist>
+            {errors.age && touched.age && <div id="feedback">{errors.age}</div>}
+            <Field component="select" id="ages" onChange={handleChange} onBlur={handleBlur} name="age" value={values.age}>
+              {ages && ages.map(x => <option key={x} value={x}>{x}</option> )}
+            </Field>
           </div>
           <div>
             <label htmlFor="sizes">Size</label>
-            <input type="text" id="sizes" list="sizes-list" />
-            <datalist id="sizes-list">
-              {sizes && sizes.map(x =>
-                <option key={x} value={x} />
-              )}
-            </datalist>
+            {errors.size && touched.size && <div id="feedback">{errors.size}</div>}
+            <Field component="select" id="sizes" onChange={handleChange} onBlur={handleBlur} name="size" value={values.size}>
+              {sizes && sizes.map(x => <option key={x} value={x}>{x}</option>)}
+            </Field>
           </div>
           <div>
             <label htmlFor="where">Where</label>
-            <input type="text" id="where" placeholder="Enter City, State or ZIP Code" />
+            {errors.where && touched.where && <div id="feedback">{errors.where}</div>}
+            <Field type="text" id="where" placeholder="Enter City, State or ZIP Code" onChange={handleChange} onBlur={handleBlur} name="where" value={values.where} />
           </div>
           <button onClick={handleOptions}>{open ? 'Less' : 'More'} Options</button>
 
@@ -70,21 +65,17 @@ const MyForm = ({ content }) => {
             <>
               <div>
                 <label htmlFor="coats">Coat</label>
-                <input type="text" id="coats" list="coats-list" />
-                <datalist id="coats-list">
-                  {type.coats && type.coats.map(x =>
-                    <option key={x} value={x} />
-                  )}
-                </datalist>
+                {errors.coat && touched.coat && <div id="feedback">{errors.coat}</div>}
+                <Field component="select" id="coats" onChange={handleChange} onBlur={handleBlur} name="coat" value={values.coat}>
+                  {type.coats && type.coats.map(x => <option key={x} value={x}>{x}</option>)}
+                </Field>
               </div>
               <div>
                 <label htmlFor="colors">Color</label>
-                <input type="text" id="colors" list="colors-list" />
-                <datalist id="colors-list">
-                  {type.colors && type.colors.map(x =>
-                    <option key={x} value={x} />
-                  )}
-                </datalist>
+                {errors.color && touched.color && <div id="feedback">{errors.color}</div>}
+                <Field component="select" id="colors" onChange={handleChange} onBlur={handleBlur} name="color" value={values.color}>
+                  {type.colors && type.colors.map(x => <option key={x} value={x}>{x}</option>)}
+                </Field>
               </div>
             </>
           }
@@ -94,8 +85,20 @@ const MyForm = ({ content }) => {
   }
 }
 
+const MyEnhancedForm = withFormik({
+  mapPropsToValues: () => ({
+    breed: '',
+    gender: '',
+    age: '',
+    size: '',
+    where: '',
+    coat: '',
+    color: ''
+  })
+})(MyForm)
+
 const mapStateToProps = (state) => ({
   content: state.content
 })
 
-export default connect(mapStateToProps, null)(MyForm)
+export default connect(mapStateToProps, null)(MyEnhancedForm)
