21ceb215004692114ac5f76eb3be551e77c78b3b
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-07-24 11:12:01 +0200

    completed Form + renamed file

---
+++ frontend/src/components/FormikForm.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react'
+import { withFormik, Field } from 'formik'
+import { connect } from 'react-redux'
+import { fetchAnimal } from '../actions'
+
+const MyForm = (props) => {
+  const [open, setOpen] = useState(false)
+  const { type, breeds, genders, ages, sizes } = props.content
+  const { values, touched, errors, handleChange, handleBlur, handleSubmit } = props
+
+  const handleOptions = (e) => {
+    e.preventDefault()
+    setOpen(!open)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="breeds">Breed</label>
+          {errors.breed && touched.breed && <div id="feedback">{errors.breed}</div>}
+          <Field component="select" id="breeds" onChange={handleChange} onBlur={handleBlur} name="breed" value={values.breed}>
+            {breeds && breeds.map(x => <option key={x.name} value={x.name}>{x.name}</option>)}
+          </Field>
+        </div>
+        <div>
+          <label htmlFor="genders">Gender</label>
+          {errors.gender && touched.gender && <div id="feedback">{errors.gender}</div>}
+          <Field component="select" id="genders" onChange={handleChange} onBlur={handleBlur} name="gender" value={values.gender}>
+            {genders && genders.map(x => <option key={x} value={x}>{x}</option>)}
+          </Field>
+        </div>
+        <div>
+          <label htmlFor="ages">Age</label>
+          {errors.age && touched.age && <div id="feedback">{errors.age}</div>}
+          <Field component="select" id="ages" onChange={handleChange} onBlur={handleBlur} name="age" value={values.age}>
+            {ages && ages.map(x => <option key={x} value={x}>{x}</option> )}
+          </Field>
+        </div>
+        <div>
+          <label htmlFor="sizes">Size</label>
+          {errors.size && touched.size && <div id="feedback">{errors.size}</div>}
+          <Field component="select" id="sizes" onChange={handleChange} onBlur={handleBlur} name="size" value={values.size}>
+            {sizes && sizes.map(x => <option key={x} value={x}>{x}</option>)}
+          </Field>
+        </div>
+        <div>
+          <label htmlFor="where">Where</label>
+          {errors.where && touched.where && <div id="feedback">{errors.where}</div>}
+          <Field type="text" id="where" placeholder="Enter City, State or ZIP Code" onChange={handleChange} onBlur={handleBlur} name="where" value={values.where} />
+        </div>
+        <button onClick={handleOptions}>{open ? 'Less' : 'More'} Options</button>
+
+        {open &&
+          <>
+            <div>
+              <label htmlFor="coats">Coat</label>
+              {errors.coat && touched.coat && <div id="feedback">{errors.coat}</div>}
+              <Field component="select" id="coats" onChange={handleChange} onBlur={handleBlur} name="coat" value={values.coat}>
+                {type.coats && type.coats.map(x => <option key={x} value={x}>{x}</option>)}
+              </Field>
+            </div>
+            <div>
+              <label htmlFor="colors">Color</label>
+              {errors.color && touched.color && <div id="feedback">{errors.color}</div>}
+              <Field component="select" id="colors" onChange={handleChange} onBlur={handleBlur} name="color" value={values.color}>
+                {type.colors && type.colors.map(x => <option key={x} value={x}>{x}</option>)}
+              </Field>
+            </div>
+          </>
+        }
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+const FormikForm = withFormik({
+
+  mapPropsToValues: () => ({
+    breed: '',
+    gender: '',
+    age: '',
+    size: '',
+    where: '',
+    coat: '',
+    color: ''
+  }),
+
+  handleSubmit: (values, { props }) => {
+    let copiedValues = {...values}
+    let selectedValues = {
+      type: props.content.type.name
+    }
+
+    for (let [key, value] of Object.entries(copiedValues)) {
+      if (value) {
+        selectedValues[key] = value
+      }
+    }
+
+    props.fetchAnimal(selectedValues)
+  }
+})(MyForm)
+
+const mapDispatchToProps = {
+  fetchAnimal
+}
+
+export default connect(null, mapDispatchToProps)(FormikForm)
