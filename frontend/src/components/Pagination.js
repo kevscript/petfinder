9f7d2a19a412537e0258768854a41f13bb8c4cb8
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-07-25 18:15:11 +0200

    completed pagination component

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,10 +1,60 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Pagination = () => {
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+  height: 80px;
+  color: ${props => props.theme.colors.primary};
+`
+
+const PrimaryButton = styled.button`
+  cursor: pointer;
+  outline: none;
+  border: 1px solid transparent;
+  display: block;
+  padding: 8px 20px;
+  border-radius: 15px;
+  font-family: 'Source Sans Pro', 'Roboto', sans-serif;
+  background: ${props => props.theme.colors.primary};
+  color: #fff;
+  transition: 0.2s;
+
+  &:disabled {
+    opacity: 0;
+    display: none;
+  }
+  
+  &:hover:enabled {
+    border: ${props => `1px solid ${props.theme.colors.primary}`};
+    background: transparent;
+    color: ${props => props.theme.colors.primary};
+    transform: scale(1.1);
+  }
+`
+
+const Pagination = ({ handlePagination, pagination }) => {
+  const { current_page, total_pages } = pagination
+
   return (
-    <div>
-      Pagination
-    </div>
+    <PaginationContainer>
+      <PrimaryButton 
+        onClick={() => handlePagination(current_page - 1)}
+        disabled={current_page === 1 ? true : false}
+      >
+        {`page ${current_page - 1}`}
+      </PrimaryButton>
+      {current_page} / {total_pages}
+      <PrimaryButton 
+        onClick={() => handlePagination(current_page + 1)}
+        disabled={current_page === total_pages ? true : false}
+      >
+        {`page ${current_page + 1}`}
+      </PrimaryButton>
+    </PaginationContainer>
   )
 }
 
