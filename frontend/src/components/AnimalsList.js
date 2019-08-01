f0fde64fa9bb6a0a64a0a4194d393c82d62982e5
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-08-01 14:58:09 +0200

    made AnimalList a flex container

--- frontend/src/components/AnimalsList.js
+++ frontend/src/components/AnimalsList.js
@@ -1,14 +1,22 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import AnimalCard from '../components/AnimalCard'
+
+const ListContainer = styled.div`
+  margin: 0 auto;
+  width: 90%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
 
 const AnimalsList = ({ data }) => {
   return (
-    <div>
+    <ListContainer>
       {data && data.map(animal => (
         <AnimalCard key={animal.id} item={animal} />
       ))}
-    </div>
+    </ListContainer>
   )
 }
 
