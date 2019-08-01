43ddf5be797ad7a4612e8babcdb25e00a5844b36
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-08-01 14:57:41 +0200

    created a mock object of animal fetched data

---
+++ frontend/src/dataMock.js
@@ -0,0 +1,375 @@
+const dataMock = [
+  {
+    "id": 45459288,
+    "organization_id": "GU01",
+    "url": "https://www.petfinder.com/dog/nikolai-45459288/gu/yigo/guam-animals-in-need-gu01/?referrer_id=0643b2b4-54df-4bc4-a9c7-2e9d6c7127ab",
+    "type": "Dog",
+    "species": "Dog",
+    "breeds": {
+      "primary": "Affenpinscher",
+      "secondary": null,
+      "mixed": false,
+      "unknown": false
+    },
+    "colors": {
+      "primary": null,
+      "secondary": null,
+      "tertiary": null
+    },
+    "age": "Baby",
+    "gender": "Male",
+    "size": "Small",
+    "coat": null,
+    "attributes": {
+      "spayed_neutered": false,
+      "house_trained": false,
+      "declawed": null,
+      "special_needs": false,
+      "shots_current": true
+    },
+    "environment": {
+      "children": null,
+      "dogs": null,
+      "cats": null
+    },
+    "tags": [],
+    "name": "Nikolai",
+    "description": null,
+    "photos": [
+      {
+        "small": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/1/?bust=1564654588&width=100",
+        "medium": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/1/?bust=1564654588&width=300",
+        "large": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/1/?bust=1564654588&width=600",
+        "full": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/1/?bust=1564654588"
+      },
+      {
+        "small": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/3/?bust=1564654590&width=100",
+        "medium": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/3/?bust=1564654590&width=300",
+        "large": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/3/?bust=1564654590&width=600",
+        "full": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/3/?bust=1564654590"
+      },
+      {
+        "small": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/2/?bust=1564654617&width=100",
+        "medium": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/2/?bust=1564654617&width=300",
+        "large": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/2/?bust=1564654617&width=600",
+        "full": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45459288/2/?bust=1564654617"
+      }
+    ],
+    "status": "adoptable",
+    "status_changed_at": "2019-08-01T10:12:27+0000",
+    "published_at": "2019-08-01T10:12:27+0000",
+    "distance": null,
+    "contact": {
+      "email": null,
+      "phone": "(671) 653-4246",
+      "address": {
+        "address1": "1/2 mile North of Our Lady of Lourdes Catholic Church",
+        "address2": "Just North of Yigo Gym",
+        "city": "Yigo",
+        "state": "GU",
+        "postcode": "96929",
+        "country": "US"
+      }
+    },
+    "_links": {
+      "self": {
+        "href": "/v2/animals/45459288"
+      },
+      "type": {
+        "href": "/v2/types/dog"
+      },
+      "organization": {
+        "href": "/v2/organizations/gu01"
+      }
+    }
+  },
+  {
+    "id": 45417211,
+    "organization_id": "GU01",
+    "url": "https://www.petfinder.com/dog/dre-45417211/gu/yigo/guam-animals-in-need-gu01/?referrer_id=0643b2b4-54df-4bc4-a9c7-2e9d6c7127ab",
+    "type": "Dog",
+    "species": "Dog",
+    "breeds": {
+      "primary": "Affenpinscher",
+      "secondary": null,
+      "mixed": false,
+      "unknown": false
+    },
+    "colors": {
+      "primary": null,
+      "secondary": null,
+      "tertiary": null
+    },
+    "age": "Baby",
+    "gender": "Male",
+    "size": "Small",
+    "coat": null,
+    "attributes": {
+      "spayed_neutered": false,
+      "house_trained": false,
+      "declawed": null,
+      "special_needs": false,
+      "shots_current": true
+    },
+    "environment": {
+      "children": null,
+      "dogs": null,
+      "cats": null
+    },
+    "tags": [],
+    "name": "Dre",
+    "description": null,
+    "photos": [
+      {
+        "small": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/1/?bust=1564654588&width=100",
+        "medium": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/1/?bust=1564654588&width=300",
+        "large": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/1/?bust=1564654588&width=600",
+        "full": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/1/?bust=1564654588"
+      },
+      {
+        "small": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/2/?bust=1564654584&width=100",
+        "medium": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/2/?bust=1564654584&width=300",
+        "large": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/2/?bust=1564654584&width=600",
+        "full": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45417211/2/?bust=1564654584"
+      }
+    ],
+    "status": "adoptable",
+    "status_changed_at": "2019-07-31T05:57:36+0000",
+    "published_at": "2019-07-31T05:57:36+0000",
+    "distance": null,
+    "contact": {
+      "email": null,
+      "phone": "(671) 653-4246",
+      "address": {
+        "address1": "1/2 mile North of Our Lady of Lourdes Catholic Church",
+        "address2": "Just North of Yigo Gym",
+        "city": "Yigo",
+        "state": "GU",
+        "postcode": "96929",
+        "country": "US"
+      }
+    },
+    "_links": {
+      "self": {
+        "href": "/v2/animals/45417211"
+      },
+      "type": {
+        "href": "/v2/types/dog"
+      },
+      "organization": {
+        "href": "/v2/organizations/gu01"
+      }
+    }
+  },
+  {
+    "id": 45416774,
+    "organization_id": "TN20",
+    "url": "https://www.petfinder.com/dog/cujo-45416774/tn/clarksville/montgomery-county-animal-care-and-control-tn20/?referrer_id=0643b2b4-54df-4bc4-a9c7-2e9d6c7127ab",
+    "type": "Dog",
+    "species": "Dog",
+    "breeds": {
+      "primary": "Labrador Retriever",
+      "secondary": "Affenpinscher",
+      "mixed": true,
+      "unknown": false
+    },
+    "colors": {
+      "primary": null,
+      "secondary": null,
+      "tertiary": null
+    },
+    "age": "Baby",
+    "gender": "Male",
+    "size": "Small",
+    "coat": null,
+    "attributes": {
+      "spayed_neutered": false,
+      "house_trained": false,
+      "declawed": null,
+      "special_needs": false,
+      "shots_current": false
+    },
+    "environment": {
+      "children": null,
+      "dogs": null,
+      "cats": null
+    },
+    "tags": [],
+    "name": "Cujo",
+    "description": null,
+    "photos": [
+      {
+        "small": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416774/1/?bust=1564655145&width=100",
+        "medium": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416774/1/?bust=1564655145&width=300",
+        "large": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416774/1/?bust=1564655145&width=600",
+        "full": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416774/1/?bust=1564655145"
+      }
+    ],
+    "status": "adoptable",
+    "status_changed_at": "2019-07-31T05:43:17+0000",
+    "published_at": "2019-07-31T05:43:17+0000",
+    "distance": null,
+    "contact": {
+      "email": "AnimalControl@mcgtn.net",
+      "phone": "(931) 648-5750",
+      "address": {
+        "address1": "616 N. Spring St.",
+        "address2": null,
+        "city": "Clarksville",
+        "state": "TN",
+        "postcode": "37040",
+        "country": "US"
+      }
+    },
+    "_links": {
+      "self": {
+        "href": "/v2/animals/45416774"
+      },
+      "type": {
+        "href": "/v2/types/dog"
+      },
+      "organization": {
+        "href": "/v2/organizations/tn20"
+      }
+    }
+  },
+  {
+    "id": 45416775,
+    "organization_id": "TN20",
+    "url": "https://www.petfinder.com/dog/patch-45416775/tn/clarksville/montgomery-county-animal-care-and-control-tn20/?referrer_id=0643b2b4-54df-4bc4-a9c7-2e9d6c7127ab",
+    "type": "Dog",
+    "species": "Dog",
+    "breeds": {
+      "primary": "Labrador Retriever",
+      "secondary": "Affenpinscher",
+      "mixed": true,
+      "unknown": false
+    },
+    "colors": {
+      "primary": null,
+      "secondary": null,
+      "tertiary": null
+    },
+    "age": "Baby",
+    "gender": "Male",
+    "size": "Small",
+    "coat": null,
+    "attributes": {
+      "spayed_neutered": false,
+      "house_trained": false,
+      "declawed": null,
+      "special_needs": false,
+      "shots_current": false
+    },
+    "environment": {
+      "children": null,
+      "dogs": null,
+      "cats": null
+    },
+    "tags": [],
+    "name": "Patch",
+    "description": null,
+    "photos": [
+      {
+        "small": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416775/1/?bust=1564655146&width=100",
+        "medium": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416775/1/?bust=1564655146&width=300",
+        "large": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416775/1/?bust=1564655146&width=600",
+        "full": "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45416775/1/?bust=1564655146"
+      }
+    ],
+    "status": "adoptable",
+    "status_changed_at": "2019-07-31T05:43:17+0000",
+    "published_at": "2019-07-31T05:43:17+0000",
+    "distance": null,
+    "contact": {
+      "email": "AnimalControl@mcgtn.net",
+      "phone": "(931) 648-5750",
+      "address": {
+        "address1": "616 N. Spring St.",
+        "address2": null,
+        "city": "Clarksville",
+        "state": "TN",
+        "postcode": "37040",
+        "country": "US"
+      }
+    },
+    "_links": {
+      "self": {
+        "href": "/v2/animals/45416775"
+      },
+      "type": {
+        "href": "/v2/types/dog"
+      },
+      "organization": {
+        "href": "/v2/organizations/tn20"
+      }
+    }
+  },
+  {
+    "id": 45413314,
+    "organization_id": "CA2377",
+    "url": "https://www.petfinder.com/dog/oppi-45413314/ca/santa-ana/bark-9-ca2377/?referrer_id=0643b2b4-54df-4bc4-a9c7-2e9d6c7127ab",
+    "type": "Dog",
+    "species": "Dog",
+    "breeds": {
+      "primary": "Affenpinscher",
+      "secondary": "Afghan Hound",
+      "mixed": true,
+      "unknown": false
+    },
+    "colors": {
+      "primary": "Sable",
+      "secondary": null,
+      "tertiary": null
+    },
+    "age": "Baby",
+    "gender": "Male",
+    "size": "Small",
+    "coat": null,
+    "attributes": {
+      "spayed_neutered": false,
+      "house_trained": false,
+      "declawed": null,
+      "special_needs": false,
+      "shots_current": false
+    },
+    "environment": {
+      "children": null,
+      "dogs": null,
+      "cats": null
+    },
+    "tags": [],
+    "name": "Oppi",
+    "description": null,
+    "photos": [],
+    "status": "adoptable",
+    "status_changed_at": "2019-07-30T19:06:25+0000",
+    "published_at": "2019-07-30T19:06:25+0000",
+    "distance": null,
+    "contact": {
+      "email": "dana@bark-9.com",
+      "phone": "949-285-1218",
+      "address": {
+        "address1": null,
+        "address2": null,
+        "city": "Santa Ana",
+        "state": "CA",
+        "postcode": "92705",
+        "country": "US"
+      }
+    },
+    "_links": {
+      "self": {
+        "href": "/v2/animals/45413314"
+      },
+      "type": {
+        "href": "/v2/types/dog"
+      },
+      "organization": {
+        "href": "/v2/organizations/ca2377"
+      }
+    }
+  }
+]
+
+export default dataMock
