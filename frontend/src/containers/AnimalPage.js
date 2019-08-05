b5d0dbdbe3e70be133895fd59e3dd7a8dea97556
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-08-05 15:45:30 +0200

    connected AnimalPage to Redux

--- frontend/src/containers/AnimalPage.js
+++ frontend/src/containers/AnimalPage.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { fetchAnimal } from '../actions'
 import styled from 'styled-components'
 
-const AnimalPage = () => {
-  return (
-    <div>
-      Animal Page
-    </div>
-  )
+const AnimalPage = ({ fetchAnimal, match, animal }) => {
+
+  const { loading, data, error } = animal
+
+  useEffect(() => {
+    const id = match.params.id
+    fetchAnimal(id)
+  }, [fetchAnimal, match.params.id])
+
+  if (loading) {
+    return <div>Loading...</div>
+  } else if (error) {
+    return <div>{error}</div>
+  } else if (data) {
+    return (
+      <div>
+        <h1>{data.name}</h1>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  animal: state.animal
+})
 
-export default connect(mapStateToProps, null)(AnimalPage)
+const mapDispatchToProps = {
+  fetchAnimal
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnimalPage)
